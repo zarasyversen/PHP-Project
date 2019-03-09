5a221f999e95bfa9cd83f49b00a9f70aa11fbd4c
Zara <zara@gene.co.uk>
2019-03-09 10:43:27 +0000

    rewrite user and password validtion

--- register.php
+++ register.php
@@ -5,73 +5,73 @@ require_once "config.php";
 //Define Variables
 $username = $password  = $confirm_password = '';
 $username_err = $password_err = $confirm_password_err = '';
+$userOk = $passwordOk = false;
+
+
+function countUsername($connection, $username){
+
+  // Prepare a select statement
+  $sql = "SELECT id FROM users WHERE username = ?";
+
+  // If we are connected and can select?
+  if($statement = mysqli_prepare($connection, $sql)){
+
+    // Bind variables to the prepared statement as parameters
+    mysqli_stmt_bind_param($statement, "s", $param_username);
+
+    // Set parameters
+    $param_username = $username;
+
+    // Try to execute the prepared statement
+    if(mysqli_stmt_execute($statement)){
+
+      // Save in DB (Store the data)
+      mysqli_stmt_store_result($statement);
+
+      // Check how many rows
+      return mysqli_stmt_num_rows($statement);
+
+    } else {
+      echo "Oops something went wrong. Please try again.";
+    }
+  }
+
+  // DOES THIS GET CLOSED IF I RETURN???? 
+  mysqli_stmt_close($stmt);
+}
+
 
 //Process Data when form is posted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-  //Validate username
-  if(empty(trim($_POST["username"]))){
+  $username = trim($_POST["username"]);
+  $password = trim($_POST["password"]);
+  $confirm_password = trim($_POST["confirm_password"]);
+
+  // Validate Username
+  if(empty($username)){
     $username_err = "Please enter a username";
-
+  } elseif(countUsername($connection, $username) >= 1) {
+    $username_err = "This username is already taken, try again.";
   } else {
-
-    // Prepare a select statement
-    $sql = "SELECT id FROM users WHERE username = ?";
-
-    // If we are connected and can select?
-    if($statement = mysqli_prepare($connection, $sql)){
-
-      // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($statement, "s", $param_username);
-
-      // Set parameters
-      $param_username = trim($_POST["username"]);
-
-      // Try to execute the prepared statement
-      if(mysqli_stmt_execute($statement)){
-        // Save in DB (Store the data)
-        mysqli_stmt_store_result($statement);
-
-        // What does this check
-        if(mysqli_stmt_num_rows($statement) == 1){
-          $username_err = "This username is already taken, try again.";
-        } else {
-          //Should I set this variable earlier? Instead of trim 3 times 
-          $username = trim($_POST["username"]);
-        }
-      } else {
-        echo "Oops something went wrong. Please try again.";
-      }
-    }
-
-    // Close Statement
-    mysqli_stmt_close($statement);
+    $userOk = true; 
   }
 
-  // Validate Password
-  if(empty(trim($_POST["password"]))){
+  // Validate Passwords
+  if(empty($password)){
     $password_err = "Please enter a password";
-  } elseif(strlen(trim($_POST["password"])) < 6){
+  } elseif(empty($confirm_password)){
+     $confirm_password_err = "Please confirm your password";
+  } elseif(strlen($password) < 6){
     $password_err = "Password must be longer than 6 characters";
+  } elseif($password != $confirm_password) {
+    $confirm_password_err = "Passwords did not match.";
   } else {
-    $password = trim($_POST["password"]);
+    $passwordOk = true;
   }
 
-  // Validate Confirm Password 
-  if(empty(trim($_POST["confirm_password"]))){
-    $confirm_password_err = "Please confirm your password";
-  } else {
-    $confirm_password = trim($_POST["confirm_password"]);
 
-    // If password is empty / if password_err is empty??
-    if(empty($password) && ($password != $confirm_password)){
-      $confirm_password_err = "Passwords did not match.";
-    }
-  }
-
-  // Check input errors before inserting data - but errors are never emptied?
-  if(empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-
+  if($userOk === true && $passwordOk === true) {
     // Prepare an INSERT statement 
     $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 
