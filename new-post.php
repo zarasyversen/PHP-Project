0d0dace436342f18d0ce653453ad5a5de992d8de
Zara <zara@gene.co.uk>
2019-03-17 16:27:12 +0000

    message post worksgit add .

--- new-post.php
+++ new-post.php
@@ -1,36 +1,81 @@
 
 <?php 
+require_once "config.php";
 $title = $content = '';
-$title_err = $content_err = '';
+$title_err = $message_err = $success = '';
+$titleOk = $messageOk = false;
 
-// Validate title - not empty
-// Validate content - not empty 
-// Prepare statement to insert post in db 
-// Add username as well 
+// Process data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $title = trim($_POST["title"]);
+  $message = trim($_POST["message"]);
+  $success = '';
 
+  if(empty($title)){
+    $title_err = "Please enter a title";
+  } else {
+    $titleOk = true;
+  }
 
+  if(empty($message)){
+    $message_err = "Please enter a message";
+  } else {
+    $messageOk = true;
+  }
 
+  if($titleOk && $messageOk){
+
+    // Prepare an INSERT statement 
+    $sql = "INSERT INTO posts (username, title, message) VALUES (?, ?, ?)";
+
+    if($statement = mysqli_prepare($connection, $sql)){
+
+      // Bind variables to prepared statement 
+      mysqli_stmt_bind_param($statement, "sss", $param_username, $param_title, $param_message);
+
+      // Set params 
+      $param_username = $_SESSION["username"];
+      $param_title = $title;
+      $param_message = $message;
+
+      // Attempt to execute statement 
+      if(mysqli_stmt_execute($statement)){
+        $success = 'Message successfully posted.';
+      } else {
+        $success = 'Something went wrong, please try again later.';
+      }
+    }
+
+    // Close statement
+    mysqli_stmt_close($statement);
+  } 
+
+  // Close connection
+  mysqli_close($connection);
+}
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" 
       method="post" 
       class="form">
       <h2>Post a message</h2>
       <div class="form__group <?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
-            <label for="post_title">Title</label>
-            <input type="text" name="post_title" id="post_title" class="form__input" value="<?php echo $title; ?>">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" class="form__input" value="<?php echo $title; ?>">
             <p class="form__error">
               <?php echo $title_err; ?>
             </p>
         </div>    
-        <div class="form__group <?php echo (!empty($content_err)) ? 'has-error' : ''; ?>">
-            <label for="post_content">Content</label>
-            <textarea id="post_content" name="post_content" rows="5" cols="33">Please enter your message here...</textarea>
+        <div class="form__group <?php echo (!empty($message_err)) ? 'has-error' : ''; ?>">
+            <label for="message">Message</label>
+            <textarea id="message" name="message" placeholder="Please enter your message here..." rows="5" cols="33"></textarea>
             <p class="form__error">
-              <?php echo $content_err;?>
+              <?php echo $message_err;?>
             </p>
         </div>
         <div class="form__group actions">
             <button type="submit" class="btn btn--primary">Submit message</button>
         </div>
-        
 </form>
+<?php if($success) :?>
+  <h3><?php echo $success; ?></h3>
+<?php endif;?>
