ee84b600cba939077444261f9a25a155f8d1f098
Zara <zara@gene.co.uk>
2019-04-07 11:04:46 +0100

    add 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php 
+$pageTitle = '404 - Page not found';
+include('header.php');
+?>
+<div class="wrapper">
+  <h1>Page not found</h1>
+  <p>Sorry about that. <a href="index.php">Go Home</a>.</p>
+</div>
