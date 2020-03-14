843dc37aa751052a6b88f891f32b6b25b8472cae
Zara <zara@gene.co.uk>
2020-03-14 15:42:56 +0000

    add route in index.php, move markup to login.php and set that to default

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
-
+  
 <?php
 $public_access = true;
 require_once("config.php");
 
-$pageTitle = 'Welcome, please log in';
-include(BASE . '/page/header.php');?>
-  <div class="wrapper">
-    <h1>Welcome to our site.</h1>
-    <?php include(BASE . '/session/message.php'); ?>
-    <?php include(BASE .'/session/login.php');?>
-  </div>
-  <?php include(BASE .'/page/footer.php');?>
-  </body>
-</html>
+$requestedUrl = $_SERVER['REQUEST_URI'];
+
+// get any slash followed by digits
+$regex = '(\/\d+)'; 
+
+// find param (any match for '/123')
+preg_match($regex, $requestedUrl, $matches);
+
+// Make param to string
+$match = implode('', $matches);
+
+// Remove param from url to find path
+$requestedPath = str_replace($match, '', $requestedUrl); 
+
+// get param but remove slash
+$param = str_replace('/', '', $match); 
+
+switch ($requestedPath) {
+  case '/profile':
+
+    $_GET['id'] = $param;
+    include(BASE . '/user/profile.php');
+    exit;
+
+    // include user/profile.php?id;
+    break;
+
+  case '/profile/avatar/create':
+    // include user/profile/avatar-upload.php;
+    break;
+
+  case '/profile/avatar/edit':
+    // include user/profile/avatar-update.php;
+    break;
+
+  case '/profile/avatar/delete':
+    // include user/profile/avatar-delete.php;
+    break;
+
+  default: 
+    include(BASE . '/session/login.php');
+}
+?>
--- session/login.php
+++ session/login.php
@@ -54,27 +54,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
 }
-?>
-<h2>Login</h2>
-<p>Please fill in your credentials to login.</p>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" 
-  method="post" class="form">
-    <div class="form__group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-        <label>Username</label>
-        <input type="text" name="username" class="form__input" value="<?php echo $username; ?>">
-        <p class="form__error">
-          <?php echo $username_err; ?>
-        </p>
-    </div>    
-    <div class="form__group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-        <label>Password</label>
-        <input type="password" name="password" class="form__input">
-        <p class="form__error">
-          <?php echo $password_err;?>
-        </p>
-    </div>
-    <div class="form__group actions">
-        <button type="submit" class="btn btn--primary">Login</button>
-    </div>
-    <p>Don't have an account? <a href="/session/register.php">Sign up now</a>.</p>
-</form>
+$pageTitle = 'Welcome, please log in';
+include(BASE . '/page/header.php');?>
+  <div class="wrapper">
+    <h1>Welcome to our site.</h1>
+    <?php include(BASE . '/session/message.php'); ?>
+    <h2>Login</h2>
+    <p>Please fill in your credentials to login.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" 
+      method="post" class="form">
+        <div class="form__group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Username</label>
+            <input type="text" name="username" class="form__input" value="<?php echo $username; ?>">
+            <p class="form__error">
+              <?php echo $username_err; ?>
+            </p>
+        </div>    
+        <div class="form__group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" class="form__input">
+            <p class="form__error">
+              <?php echo $password_err;?>
+            </p>
+        </div>
+        <div class="form__group actions">
+            <button type="submit" class="btn btn--primary">Login</button>
+        </div>
+        <p>Don't have an account? <a href="/session/register.php">Sign up now</a>.</p>
+    </form>
+  </div>
+  <?php include(BASE .'/page/footer.php');?>
