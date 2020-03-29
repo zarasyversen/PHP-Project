3f512f6eed02dadd4f49d8e7ed8e013e435c9d47
Zara <zara@gene.co.uk>
2020-03-29 11:52:29 +0100

    rename controllers, one for each view

---
+++ Controller/Profile/Avatar/Delete.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Controller\Profile\Avatar;
+
+class Delete {
+
+  public static function view() {
+    return '/user/profile/avatar-delete.php';
+  }
+
+}
---
+++ Controller/Profile/Avatar/Edit.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Controller\Profile\Avatar;
+
+class Edit {
+
+  public static function view() {
+    return '/user/profile/avatar-update.php';
+  }
+
+}
