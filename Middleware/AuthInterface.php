185268cc7be58fedca5911e67c05d00f82ef6ec3
Zara <zara@gene.co.uk>
2020-05-17 10:29:31 +0100

    remove comment about interface naming

--- Middleware/AuthInterface.php
+++ Middleware/AuthInterface.php
@@ -1,11 +1,6 @@
 <?php
 namespace Middleware;
 
-//
-// Interface can not be called interface.php lol
-// Needs to have the same filename as interface name.
-// Parse error: syntax error, unexpected 'Interface' (T_INTERFACE) when implements
-//
 interface AuthInterface {
   
   public function execute();
