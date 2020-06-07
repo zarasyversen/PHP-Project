13ecd75b4c3a69a6da664ba3205254ed121f0d40
Zara <zara@gene.co.uk>
2020-06-07 12:50:27 +0100

    recommit profile to move to uppercase folder

---
+++ Controller/Profile.php
@@ -0,0 +1,34 @@
+<?php 
+namespace Controller;
+
+use Repository\UserRepository;
+use Repository\PostRepository;
+use Helper\Session as Session;
+
+/**
+ * Profile Controller
+ */
+class Profile extends \Controller\Base {
+
+  public function view($id) {
+
+    try {
+      $user = UserRepository::getUserById($id);
+      $posts = new PostRepository();
+      $postList = $posts->getAllUserPosts($user->getId());
+      $canEdit = $user->canEditUser();
+    } catch (\Exceptions\NoPermission $e) {
+      $canEdit = false;
+    }
+
+    $this->displayTemplate(
+      '/user/profile',
+      [
+        'user' => $user,
+        'canEdit' => $canEdit,
+        'postList' => $postList
+      ]
+    );
+  }
+
+}
