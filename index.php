8bdf8d5c5be22c70a85e9dac4bff805c3e050590
Zara <zara@gene.co.uk>
2020-06-14 21:28:24 +0100

    add 404 page

--- index.php
+++ index.php
@@ -3,5 +3,11 @@ require_once("config.php");
 
 $requestedUrl = $_SERVER['REQUEST_URI'];
 
-return Helper\Route::get($requestedUrl);
+try {
+  Helper\Route::get($requestedUrl);
+} catch(\Exceptions\NotFound | \Exception $e) {
+  http_response_code(400);
+  $pageTitle = "Page Not Found";
+  include(BASE . '/view/page/404.php');
+}
 ?>
